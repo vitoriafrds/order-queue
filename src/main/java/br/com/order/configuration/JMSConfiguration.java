85e623dc3f53cea4110a9f1e94b6a75f561f7f07
vitoriafrds <vit.ferreiradossantos@gmail.com>
2020-07-29 17:52:11 -0300

    configuracoes iniciais

---
+++ src/main/java/br/com/order/configuration/JMSConfiguration.java
@@ -0,0 +1,56 @@
+package br.com.order.configuration;
+
+import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.auth.STSAssumeRoleSessionCredentialsProvider;
+import com.amazonaws.internal.StaticCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.destination.DynamicDestinationResolver;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.Session;
+
+@Configuration
+@EnableJms
+public class JMSConfiguration {
+
+    @Value("${amazon.accessKey}")
+    private String accessKey;
+
+    @Value("${amazon.secretKey}")
+    private String secretKey;
+
+    @Autowired
+    private SQSConnectionFactory connectionFactory;
+
+    @Bean
+    public SQSConnectionFactory createConnectionFactory() {
+
+        return SQSConnectionFactory.builder().withRegion(Region.getRegion(Regions.SA_EAST_1))
+                .withAWSCredentialsProvider(new StaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey))).build();
+    }
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListener() {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(this.connectionFactory);
+        factory.setDestinationResolver(new DynamicDestinationResolver());
+        factory.setSessionAcknowledgeMode(Session.AUTO_ACKNOWLEDGE);
+
+        return factory;
+    }
+
+    @Bean
+    public JmsTemplate defaultJmsTemplate(){
+        return new JmsTemplate(this.connectionFactory);
+    }
+}
---
+++ src/test/java/br/com/order-queue/AppTest.java
@@ -0,0 +1,20 @@
+package br.com.order-queue;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
